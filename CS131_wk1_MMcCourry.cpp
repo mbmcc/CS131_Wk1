7b83758be59ca660059efed51fe499a8f0695886
Matthew <34489345+mbmcc@users.noreply.github.com>
2018-04-02 21:29:38 -0700

    Tried to remove std::
    Per assignment

---
+++ CS131_wk1_MMcCourry.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ //Compiler version g++ 6.3.0
+
+
+int  main()
+{
+
+      int  age;
+
+     cout <<  "Enter your age:" << std::  endl;
+     cin >> age;
+     cout <<  "\nYour age is: "  <<age;
+
+      return 0;
+
+}
