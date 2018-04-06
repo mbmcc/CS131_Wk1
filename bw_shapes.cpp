9bdfde3cd1ae53cb2885ab2a77c3509a6fb12182
Matthew <34489345+mbmcc@users.noreply.github.com>
2018-04-06 15:23:56 -0700

    Create bw_shapes.cpp

---
+++ bw_shapes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/* make a program that outputs some shapes on the screen using aterix pg63
+*/
+
+int main()
+{
+  std::cout << "\
+* * * * * *    * * *       *         *      \n\
+*         *   *     *    * * *      * *     \n\
+*         *  *       *  * * * *    *   *    \n\
+*         *  *       *     *      *     *   \n\
+*         *  *       *     *     *       *  \n\
+*         *  *       *     *      *     *   \n\
+*         *  *       *     *       *   *    \n\
+*         *   *     *      *        * *     \n\
+* * * * * *    * * *       *         *      \n\
+  ";
+    return 0;
+}
